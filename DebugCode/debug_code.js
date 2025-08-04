e24462e3fa7d1c7b89bb74feccc293fc10fad72c
JAKe6699 <jondular72@yahoo.com>
2025-08-04 15:17:18 -0400

    adding debugger files

---
+++ DebugCode/debug_code.js
@@ -0,0 +1,49 @@
+    
+    function performOperation(oper) {
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+    // Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+        let result = 0;
+        
+        // Perform the operation
+        switch (oper) {
+            case 'add':
+                result=add(num1, num2);
+                break;
+            case 'mutiply':
+                result=multiply(num1, num2);
+            break;
+            default:
+                break;
+        }
+        // Display the result
+        displayResult(result);
+    } else {
+        displayResult('Please enter valid numbers');
+                }
+    }
+
+    function add(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // Add the numbers
+                return a + b;
+    }
+
+    function multiply(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // Multiply the numbers
+                return a * b;
+    }
+
+    function displayResult(result) {
+                // Display the result in the paragraph element
+                const resultElement = document.getElementById('result');
+                resultElement.textContent = `The result is: ${result}`;
+    }
+        
